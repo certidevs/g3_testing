73b63af166715767860d83a29db8cc88df8bbbf3
Mohamed Afallah <mohaparla184@gmail.com>
2026-04-27 14:59:24 +0200

    Closes #7 : he creado el MesssageRepository.

---
+++ src/main/java/com/demo/repositories/MessageRepository.java
@@ -0,0 +1,9 @@
+package com.demo.repositories;
+
+import com.demo.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+}
