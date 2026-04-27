02dad2c7b517f9d01d03f3e61c5a95b3248fd452
Rubén Pinto <angel_rubenp@hotmail.com>
2026-04-27 15:14:19 +0200

    He realizado la entidad Amenity

---
+++ src/main/java/com/demo/model/Amenity.java
@@ -0,0 +1,39 @@
+package com.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+//import org.apache.catalina.User;
+
+import java.time.LocalDateTime;
+
+/**
+ * Entidad que representa una propiedad publicada en la plataforma.
+ * */
+
+@Entity
+@Table(name = "listings")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Amenity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //@Column(nullable = false)
+    private String name;
+    @Column(length=500) //aumenta espacio a 500 caracteres vs 256 por defecto
+    private String description;
+    @Column(columnDefinition="TEXT") //permite almacenar textos largos sin límite de caracteres, ideal para descripciones detalladas
+    private String icon;
+
+    //@ManyToOne
+    //private User user;
+
+
+
+}
