65b2d459308bec7c24c9933df92ad2cdc0611d29
Mohamed Afallah <mohaparla184@gmail.com>
2026-04-27 16:26:50 +0200

    Closes #7 : he modificado el MesssageRepository

--- src/main/java/com/demo/repositories/MessageRepository.java
+++ src/main/java/com/demo/repositories/MessageRepository.java
@@ -4,6 +4,12 @@ import com.demo.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findByIsReadFalse();
+    List<Message> findBySentAtBetween(String startDate, String endDate);
+    List<Message> findBySentAtOrderAsc();
+    List<Message> findBySentAtOrderDesc();
 }
