6077106496ad596b441c903df2af40e9048c71b2
Mohamed Afallah <mohaparla184@gmail.com>
2026-04-28 15:41:08 +0200

    #7: He modificado el MessageRepository, y he testeado la recuperacion de una conversacion de un booking

--- src/main/java/com/demo/repositories/MessageRepository.java
+++ src/main/java/com/demo/repositories/MessageRepository.java
@@ -1,6 +1,7 @@
 package com.demo.repositories;
 
 import com.demo.model.Message;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,8 +10,5 @@ import java.util.List;
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
-    //List<Message> findByIsReadFalse();
-    //List<Message> findBySentAtBetween(String startDate, String endDate);
-    //List<Message> findBySentAtOrderAsc();
-    //List<Message> findBySentAtOrderDesc();
+    List<Message> findByConversationId(Long conversationId, Sort sort);
 }
