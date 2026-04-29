2ade4c3eb1a3b32b95f1e559f3d6214a365b513d
javiersar <javier.sar.pedroza@gmail.com>
2026-04-29 12:42:11 +0200

    #11
    #11 ListingRepository
    *3 métodos
    -Lista de ofertas activas
    -Lista por rango de precio
    - Lista por máxima capacidad de invitados

--- src/main/java/com/demo/repositories/ListingRepository.java
+++ src/main/java/com/demo/repositories/ListingRepository.java
@@ -3,5 +3,12 @@ package com.demo.repositories;
 import com.demo.model.Listing;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ListingRepository extends JpaRepository<Listing, Long> {
+
+    List<Listing> findByIsActiveTrue();
+    List<Listing> findByIsActiveTrueAndPricePerNightBetween(Double minPrice, Double maxPrice);
+    List<Listing> findByIsActiveAndMaxGuestsGreaterThanEqual(Boolean isActive, Integer maxGuests);
+
 }
