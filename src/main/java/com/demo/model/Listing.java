4799d7b40c2fc7d397cd05e10ddcda276fd157fc
javiersar <javier.sar.pedroza@gmail.com>
2026-04-29 12:44:46 +0200

    #2 Eliminación de comentarios y ajuste en atributo longDescription cambio de "TEXT" a lenght 10000

--- src/main/java/com/demo/model/Listing.java
+++ src/main/java/com/demo/model/Listing.java
@@ -24,18 +24,18 @@ public class Listing {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //@Column(nullable = false)
     private String title;
+
     @Column(length=500) //aumenta espacio a 500 caracteres vs 256 por defecto
     private String shortDescription;
-    @Column(columnDefinition="TEXT") //permite almacenar textos largos sin límite de caracteres, ideal para descripciones detalladas
+
+    @Column(length=10000)
     private String longDescription;
 
     private Double pricePerNight;
     private Integer minNights;
     private Integer maxNights;
     private Integer maxGuests;
-
 
     @Builder.Default
     private Boolean isActive = true;
