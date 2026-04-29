5eb8da46319171c9b4fb4eb616bbe6b057c6ddbe
Joseph <Simonfuente88@gmail.com>
2026-04-29 18:41:28 +0200

    test precios

--- src/main/java/com/demo/repositories/AddonRepository.java
+++ src/main/java/com/demo/repositories/AddonRepository.java
@@ -1,8 +1,17 @@
 package com.demo.repositories;
 
 import com.demo.model.Addon;
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AddonRepository extends JpaRepository<Addon, Long> {
+import java.util.List;
 
+public interface AddonRepository extends JpaRepository<Addon, Long> {
+    List<Addon> findByPrice(Double price);
+
+    List<Addon> findByTitle(String title);
+    List<Addon> findByDescription(String description);
+
+    @NonNull
+    List<Addon> findAll();
 }
