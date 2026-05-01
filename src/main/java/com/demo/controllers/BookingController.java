ab55b1537ecc4cc331d3115a61543154debb3330
Gallego <nicogallego15@gmail.com>
2026-05-01 15:18:19 +0200

    he creado reviewController y sus dos html correspondientes

--- src/main/java/com/demo/controllers/BookingController.java
+++ src/main/java/com/demo/controllers/BookingController.java
@@ -3,10 +3,12 @@ package com.demo.controllers;
 
 import com.demo.repositories.BookingRepository;
 import com.demo.repositories.ListingRepository;
+import com.demo.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @AllArgsConstructor
 @Controller
@@ -14,14 +16,25 @@ public class BookingController {
 
     private final BookingRepository bookingRepository;
     private final ListingRepository listingRepository;
+    private final UserRepository userRepository;
 
     @GetMapping("/bookings")
     public String bookings(Model model) {
 
         model.addAttribute("bookings", bookingRepository.findAll());
-        //model.addAttribute("listings", listingRepository.findAll());
+        model.addAttribute("listings", listingRepository.findAll());
 
         return "booking/booking-list";
+
+    }
+
+@GetMapping("booking/{id}")
+    public String bookingDetail(Model model, @PathVariable Long id) {
+
+        model.addAttribute("booking", bookingRepository.findById(id).orElseThrow());
+        model.addAttribute("listing", listingRepository.findById(id).orElseThrow());
+
+        return "booking/booking-detail";
 
     }
 
