ff20142893c9392ab924df3277d6d20327935c5a
nicogallego <nicogallego15@gmail.com>
2026-05-05 15:22:55 +0200

    delete review

--- src/main/java/com/demo/controllers/ReviewController.java
+++ src/main/java/com/demo/controllers/ReviewController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @AllArgsConstructor
 @Controller
@@ -29,6 +30,13 @@ public class ReviewController {
         model.addAttribute("review", reviewRepository.findById(id));
         model.addAttribute("booking", bookingRepository.findById(id));
         return "review/review-detail";
+    }
+
+    @GetMapping("reviews/delete/{id}")
+    public String delete(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        reviewRepository.deleteById(id);
+        redirectAttributes.addFlashAttribute("message", "Borrado exitosamente");
+        return "redirect:/reviews";
     }
 
 
