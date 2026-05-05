649a62863b16ad15aae1227e328d7ea1ad6ee005
nicogallego <nicogallego15@gmail.com>
2026-05-05 15:56:06 +0200

    delete review en ReviewControllerTest

--- src/test/java/com/demo/controllers/ReviewControllerTest.java
+++ src/test/java/com/demo/controllers/ReviewControllerTest.java
@@ -1,7 +1,66 @@
 package com.demo.controllers;
 
+import com.demo.model.Booking;
+import com.demo.model.Review;
+import com.demo.model.enums.BookingStatus;
+import com.demo.repositories.BookingRepository;
+import com.demo.repositories.ReviewRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 class ReviewControllerTest {
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    @Autowired
+    BookingRepository bookingRepository;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    Review rev1;
+
+    Booking b1;
+
+    @BeforeEach
+    void setUp(){
+        reviewRepository.deleteAll();
+        bookingRepository.deleteAll();
+
+        b1= Booking.builder().status(BookingStatus.CONFIRMED).build();
+
+        bookingRepository.save(b1);
+
+        rev1= Review.builder().creationDate(LocalDate.now()).booking(b1).build();
+        reviewRepository.save(rev1);
+    }
+
+    @Test
+    void detectarReview(){
+        assertTrue(reviewRepository.findById(rev1.getId()).isPresent());
+    }
+
+    @Test
+    void eliminarReview(){
+        reviewRepository.deleteById(rev1.getId());
+        assertFalse(reviewRepository.findById(rev1.getId()).isPresent());
+    }
+
+
+
+
 
 }
