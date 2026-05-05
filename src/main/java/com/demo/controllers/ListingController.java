35bcf9ed8cb64d46304525b7d3b10cbbea16a2dc
javiersar <javier.sar.pedroza@gmail.com>
2026-05-05 18:37:11 +0200

    #16 feat(listing): enable functional list and detail views

--- src/main/java/com/demo/controllers/ListingController.java
+++ src/main/java/com/demo/controllers/ListingController.java
@@ -1,8 +1,14 @@
 package com.demo.controllers;
 
+import com.demo.model.Listing;
 import com.demo.repositories.ListingRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequestMapping("/listings")
@@ -13,5 +19,22 @@ class ListingController {
     public ListingController(ListingRepository listingRepository) {
         this.listingRepository = listingRepository;
     }
+
+    @GetMapping
+    public String list(Model model){
+        model.addAttribute("listings", listingRepository.findAll());
+        return "listing/listing-list";
+    }
+
+    @GetMapping("/{id}")
+    public String detail(@PathVariable Long id, Model model) {
+        Listing listing = listingRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        model.addAttribute("listing", listing);
+        return "listing/listing-detail";
+    }
+
+
 
 }
