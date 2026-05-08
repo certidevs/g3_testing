1d0b2d91cfc604283c7e60c64317925abf079ac4
Rubén Pinto <angel_rubenp@hotmail.com>
2026-05-08 16:05:29 +0200

    He realizado Amenity controller test

--- src/test/java/com/demo/controllers/AmenityControllerTest.java
+++ src/test/java/com/demo/controllers/AmenityControllerTest.java
@@ -2,7 +2,6 @@ package com.demo.controllers;
 
 import com.demo.model.Amenity;
 import com.demo.repositories.AmenityRepository;
-import com.demo.repositories.ListingRepository;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -27,79 +26,55 @@ public class AmenityControllerTest {
 
     @Autowired private MockMvc mockMvc;
     @Autowired private AmenityRepository amenityRepository;
-    @Autowired private ListingRepository listingRepository;
 
 
     @BeforeEach
     void setUp() {
 
-        // crear restaurantes demo
         amenityRepository.deleteAll();
-        listingRepository.deleteAll();
 
         amenityRepository.saveAll(List.of(
                 Amenity.builder().name("Piscina").build(),
                 Amenity.builder().name("Atención a la Habitación").build(),
                 Amenity.builder().name("Restaurant").build()
         ));
-        // crear amenities demo si se fueran a usar en varios métodos de test
 
     }
 
     @Test
     void amenityFull() throws Exception {
-        // invocar endpoint http://localhost:8080/restaurants
-        // se lanza una petición HTTP GET al controlador /restaurants
-        // y verificamos que devuelve un status 200 OK
         mockMvc.perform(get("/amenity"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("amenity/amenity-list"))
-                .andExpect(model().attributeExists("amenity"))
-                .andExpect(model().attribute("amenity", hasSize(3)));
+                .andExpect(model().attributeExists("amenities"))
+                .andExpect(model().attribute("amenities", hasSize(3)));
     }
     @Test
-    void restaurantsEmpty() throws Exception {
+    void amenitiesEmpty() throws Exception {
 
         amenityRepository.deleteAll();
 
-        mockMvc.perform(get("/restaurants"))
+        mockMvc.perform(get("/amenity"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("restaurants/restaurant-list"))
-                .andExpect(model().attributeExists("restaurants"))
-                .andExpect(model().attribute("restaurants", hasSize(0)));
+                .andExpect(view().name("amenity/amenity-list"))
+                .andExpect(model().attributeExists("amenities"))
+                .andExpect(model().attribute("amenities", hasSize(0)));
     }
 
     @Test
-    void restaurantDetailIsPresentTrue() throws Exception {
+    void amenityDetailIsPresentTrue() throws Exception {
 
-        Amenity restaurant = amenityRepository.findAll().getFirst();
-        Long restaurantId = restaurant.getId();
+        Amenity amenity = amenityRepository.findAll().getFirst();
+        Long amenityId = amenity.getId();
 
-
-        mockMvc.perform(get("/restaurants/" + restaurantId))
+        mockMvc.perform(get("/amenity/" + amenity.getId()))
                 .andExpect(status().isOk())
-                .andExpect(view().name("restaurants/restaurant-detail"))
-                .andExpect(model().attributeExists("restaurant"))
-                .andExpect(model().attributeExists("dishes"))
-                .andExpect(model().attribute("restaurant", hasProperty("id", is(restaurantId))))
-                .andExpect(model().attribute("restaurant", hasProperty("name", is(restaurant.getName()))));
+                .andExpect(view().name("amenity/amenity-detail"))
+                .andExpect(model().attributeExists("amenity"))
+                .andExpect(model().attribute("amenity", hasProperty("id", is(amenityId))))
+                .andExpect(model().attribute("amenity", hasProperty("name", is(amenity.getName()))));
+
     }
-
-    @Test
-    void restaurantDetailIsPresentFalse()  throws Exception{
-        // buscar un restaurante que no exista y comprobar que hace un redirect
-
-        Long idInexistente = 99999L;
-
-        mockMvc.perform(get("/restaurants/" + idInexistente))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/restaurants"));
-    }
-
-
-
-
-
 
 }
 
