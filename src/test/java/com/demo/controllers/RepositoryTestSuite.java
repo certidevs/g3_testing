e64d3024309adeb18f7db25648458b59e8754703
javiersar <javier.sar.pedroza@gmail.com>
2026-05-12 16:40:32 +0200

    #20 Deleted RepositoryTestSuite because it was just created

--- src/test/java/com/demo/controllers/RepositoryTestSuite.java
+++
@@ -1,8 +0,0 @@
-package com.demo.controllers;
-
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.suite.api.Suite;
-@Suite
-@SelectPackages("com.demo.repositories")
-public class RepositoryTestSuite {
-}
