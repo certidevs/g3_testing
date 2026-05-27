ea30e7e54c5dfe8b8838cf957714e07ceb4107d3
alansastre <alan@certidevs.com>
2026-05-27 18:10:51 +0200

    github actions worlflow + testing

--- src/test/java/com/demo/controllers/BookingControllerTest.java
+++ src/test/java/com/demo/controllers/BookingControllerTest.java
@@ -4,6 +4,7 @@ import com.demo.model.Booking;
 import com.demo.model.Listing;
 import com.demo.model.User;
 import com.demo.model.enums.BookingStatus;
+import com.demo.model.enums.Role;
 import com.demo.repositories.BookingRepository;
 import com.demo.repositories.ListingRepository;
 import com.demo.repositories.UserRepository;
@@ -20,6 +21,8 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -53,15 +56,27 @@ class BookingControllerTest {
         listingRepository.deleteAll();
         bookingRepository.deleteAll();
 
-        user = User.builder().name("Juan").email("juanito@gmail.com").build();
+        user = User.builder()
+                .name("Juan")
+                .email("juanito@gmail.com")
+                .username("juan")
+                .password("$2a$10$JB44S6i2C4t67VquEOpiIua1lDDXmLv54NDnnCKBoC/E6WjHyk.Ei")
+                .role(Role.ROLE_ADMIN)
+                .build();
         userRepository.save(user);
 
         apartamento = Listing.builder().title("Casa en la playa").isActive(true).owner(user).maxGuests(5).minNights(2).maxNights(10).registeredAt(LocalDateTime.now()).pricePerNight(25.0).build();
         listingRepository.save(apartamento);
 
-        b1= Booking.builder().listing(apartamento).status(BookingStatus.CONFIRMED).checkIn(LocalDateTime.of(2026,4,22,15,30)).checkOut(LocalDateTime.of(2026,4,26,15,30)).build();
-        b2= Booking.builder().listing(apartamento).status(BookingStatus.PENDING).checkIn(LocalDateTime.of(2026,3,22,15,30)).checkOut(LocalDateTime.of(2026,3,26,15,30)).build();
-        b3 = Booking.builder().listing(apartamento).status(BookingStatus.PENDING).checkIn(LocalDateTime.now()).checkOut(LocalDateTime.now()).build();
+        b1= Booking.builder()
+                .listing(apartamento)
+                .status(BookingStatus.CONFIRMED)
+                .checkIn(LocalDateTime.of(2026,4,22,15,30))
+                .checkOut(LocalDateTime.of(2026,4,26,15,30))
+                .guest(user)
+                .build();
+        b2= Booking.builder().guest(user).listing(apartamento).status(BookingStatus.PENDING).checkIn(LocalDateTime.of(2026,3,22,15,30)).checkOut(LocalDateTime.of(2026,3,26,15,30)).build();
+        b3 = Booking.builder().guest(user).listing(apartamento).status(BookingStatus.PENDING).checkIn(LocalDateTime.now()).checkOut(LocalDateTime.now()).build();
         List<Booking> ap = List.of(b1,b2,b3);
         bookingRepository.saveAll(ap);
 
@@ -70,7 +85,7 @@ class BookingControllerTest {
     @Test
     void detectarBooking() throws Exception{
         assertTrue(bookingRepository.findById(b1.getId()).isPresent());
-        mockMvc.perform(get("/bookings/" + b1.getId()))
+        mockMvc.perform(get("/bookings/" + b1.getId()).with(user(user)))
                 .andExpect(status().isOk())
                 .andExpect(view().name("booking/booking-detail"))
                 .andExpect(model().attributeExists("booking"))
@@ -81,7 +96,7 @@ class BookingControllerTest {
     @Test
     void listaBooking() throws Exception{
         assertFalse(bookingRepository.findAll().isEmpty());
-        mockMvc.perform(get("/bookings"))
+        mockMvc.perform(get("/bookings").with(user(user)))
                 .andExpect(status().isOk())
                 .andExpect(view().name("booking/booking-list"))
                 .andExpect(model().attributeExists("bookings"))
@@ -95,7 +110,8 @@ class BookingControllerTest {
     void borrarBooking() throws Exception{
         assertTrue(bookingRepository.findById(b1.getId()).isPresent());
 
-        mockMvc.perform(post("/bookings/"+b1.getId()+"/delete"))
+        mockMvc.perform(post("/bookings/"+b1.getId()+"/delete")
+                        .with(csrf()).with(user(user)))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/bookings"))
                 .andExpect(flash().attributeExists("message"))
@@ -109,7 +125,7 @@ class BookingControllerTest {
     void confirmarBooking()throws Exception{
         assertTrue(bookingRepository.findById(b2.getId()).isPresent());
 
-        mockMvc.perform(post("/bookings/"+b2.getId()+"/confirm"))
+        mockMvc.perform(post("/bookings/"+b2.getId()+"/confirm").with(csrf()).with(user(user)))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/bookings/"+b2.getId()))
                 .andExpect(flash().attributeExists("message"))
@@ -123,7 +139,7 @@ class BookingControllerTest {
     void cancelarBooking()throws Exception{
         assertTrue(bookingRepository.findById(b3.getId()).isPresent());
 
-        mockMvc.perform(post("/bookings/"+b3.getId()+"/cancel"))
+        mockMvc.perform(post("/bookings/"+b3.getId()+"/cancel").with(csrf()).with(user(user)))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/bookings/"+b3.getId()))
                 .andExpect(flash().attributeExists("message"))
@@ -139,6 +155,7 @@ class BookingControllerTest {
         long bookingsAntes = bookingRepository.count();
 
         mockMvc.perform(post("/bookings")
+                        .with(csrf()).with(user(user))
                         .param("listing.id", String.valueOf(apartamento.getId()))
                         .param("checkIn", "2026-06-01")
                         .param("checkOut", "2026-06-05"))
