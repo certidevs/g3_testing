6e835e3ac8ba4619e9ae596dd496f95f5c78d491
javiersar <javier.sar.pedroza@gmail.com>
2026-06-02 22:30:06 +0200

    Creacion de Clase BaseSeleniumTest

---
+++ src/test/java/com/demo/ui/BaseSeleniumTest.java
@@ -0,0 +1,9 @@
+package com.demo.ui;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BaseSeleniumTest {
+}
