21a9caaab8a462da331ea617fbd752cf0d69d9a5
Gallego <nicogallego15@gmail.com>
2026-06-03 05:08:25 +0200

    rellenando la calse BaseSeleniumTest

--- src/test/java/com/demo/ui/BaseSeleniumTest.java
+++ src/test/java/com/demo/ui/BaseSeleniumTest.java
@@ -1,9 +1,60 @@
 package com.demo.ui;
 
+import com.demo.model.*;
+import com.demo.repositories.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BaseSeleniumTest {
+
+    @LocalServerPort
+    int port;
+    @Autowired
+    ListingRepository listingRepository;
+    @Autowired
+    BookingRepository bookingRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    ReviewRepository reviewRepository;
+    @Autowired
+    MessageRepository messageRepository;
+    @Autowired
+    ConversationRepository conversationRepository;
+    @Autowired
+    AmenityRepository amenityRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    String baseUrl;
+    WebDriver driver;
+    WebDriverWait wait;
+
+    User currentUser,adminUser,hostUser;
+    Listing listing;
+    Booking booking;
+    Review review;
+    Message message;
+    Conversation conversation;
+    Amenity amenity;
+
+    @BeforeEach
+    void setUp(){
+        reviewRepository.deleteAll();
+        bookingRepository.deleteAll();
+        listingRepository.deleteAll();
+        userRepository.deleteAll();
+        messageRepository.deleteAll();
+        conversationRepository.deleteAll();
+        amenityRepository.deleteAll();
+
+
+    }
+
 }
